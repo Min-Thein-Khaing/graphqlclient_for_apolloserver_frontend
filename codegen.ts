8e591c3ebf707071fdd60c5a7e0798c757f99bc9
Min-Thein-Khaing <nosoonerthan1@gmail.com>
2026-04-17 11:02:28 +0700

    graphql-client-setup

---
+++ codegen.ts
@@ -0,0 +1,21 @@
+import { CodegenConfig } from "@graphql-codegen/cli";
+
+
+const config: CodegenConfig = {
+  schema: "http://localhost:4000",
+  documents: ["src/**/*.tsx", "src/**/*.ts"],
+  generates: {
+    "./src/__generate__/": {
+      preset: "client",
+      presetConfig: {
+        gqlTagName: "gql"
+      }
+    },
+    "./src/__generate__/types.ts": {
+      plugins: ["typescript", "typescript-operations"]
+    }
+  },
+  ignoreNoDocuments: true
+};
+
+export default config;
